73af9ba8a1fa80a644c9675fd03a38d47627535f
zhangmin <zhangmin@czb365.com>
2019-12-04 17:56:06 +0800

    add test2.py

---
+++ test2.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8 -*-
+
+
+name1 = {'name':'zhangsan','old':'20'}
+name2 = {'name':'lisi','old':'17'}
+name3 = {'name':'lisa','old':'30'}
+names = [name1,name2,name3]
+for name  in names:
+    print (name)
