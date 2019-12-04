7b83f5249aecd3c077aa7e44e012bafb12eb4a91
zhangmin <zhangmin@czb365.com>
2019-12-04 18:19:41 +0800

    add fix2

--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 # -*-coding:utf-8 -*-
-
+#this is the second fix.
 
 name1 = {'name':'zhangsan','old':'20'}
 name2 = {'name':'lisi','old':'17'}
@@ -7,3 +7,6 @@ name3 = {'name':'lisa','old':'30'}
 names = [name1,name2,name3]
 for name  in names:
     print (name)
+
+#this is the first fix.
+
